28f219548cc9a216c68aeb969dbd0da225faf505
mat105 <mat.105@hotmail.com>
2016-04-05 16:22:10 -0300

    Agregado listado de jugadores, acceso a jugadores individuales y templates para mostarlos.

--- cliente.py
+++ cliente.py
@@ -1,55 +1,104 @@
-from Flask import Flask, url_for
+from flask import Flask, request, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
 
 class Ojeo:
-	CODIGO = 0
+    CODIGO = 0
 
-	def __init__(self, jugador, fecha, comentarios):
-		self.comentarios = comentarios
-		self.fecha = fecha
-		self.codigo = Ojeo.CODIGO
-		
-		Ojeo.CODIGO+=1
+    def __init__(self, jugador, fecha, comentarios):
+        self.comentarios = comentarios
+        self.fecha = fecha
+        self.codigo = Ojeo.CODIGO
+
+        Ojeo.CODIGO+=1
+
 
 class Jugador:
-	CODIGO = 0
+    CODIGO = 0
 
-	def agregar_ojeo(self, ojo):
-		self.ojeos.append(ojo)
+    def __str__(self):
+        return "Nombre: {}      Club: {}        Posicion: {}        Costo: {}".format( self.nombre, self.club, self.posicion, self.costo )
+        
+    def __repr__(self):
+        return "Nombre: {}      Club: {}".format(self.nombre, self.club)
 
-	def __init__(self, nombre, club, posicion, costo):
-		self.nombre = nombre
-		self.club = club
-		self.posicion = posicion
-		self.costo = costo
-		self.ojeos = []
-		self.codigo = Jugador.CODIGO
-		
-		Jugador.CODIGO+=1
+    def agregar_ojeo(self, ojo):
+        self.ojeos.append(ojo)
+
+    def __init__(self, nombre, club, posicion, costo):
+        self.nombre = nombre
+        self.club = club
+        self.posicion = posicion
+        self.costo = costo
+        self.ojeos = []
+        self.codigo = Jugador.CODIGO
+
+        Jugador.CODIGO+=1
 		
 
 # A remover
-personas = {}
+personas = {
+    0:Jugador("Cebolla", "Independiente", "Volante", 10000000),
+    1:Jugador("Messi", "Barcelona", "Delantero", 80000000),
+    2:Jugador("Robben", "Bayern Munich", "Delantero", 30312424)
+    }
+    
 ojeos = {}
 		
+@app.route('/')
 def index():
-	pass
+    return "Bienvenido"
 	
 @app.route('/jugador')
 def jugadores():
-	return personas
+    return render_template('jugador_listado.html', listado = personas)
 	
-@app.route('/jugador/<int:cod_id>')
+    
+def guardar_sqlite(cod_id, jug):
+    personas[cod_id] = jug
+    
+@app.route('/jugador/<int:cod_id>', methods=['GET', 'POST'])
 def jugador(cod_id):
-	return personas[cod_id]
+    jug = personas.get(cod_id, None)
+    nom = club = posicion = costo = None
+    success=0
+    
+    
+    if request.method == 'POST':
+        nom = request.args.get('nombre',None)
+        club = request.args.get('club', None)
+        posicion = request.args.get('posicion', None)
+        costo = request.args.get('costo', None)
+        
+        if not jug and nom and club and posicion and costo:
+            jug = Jugador(nom, club, posicion, costo)
+            guardar_sqlite(cod_id, jug)
+        elif jug:
+            success = 1 # Ya existe
+        else:
+            success = 2 # Parametros insuficientes
+    else:
+        if jug:
+            nom = jug.nombre
+            club = jug.club
+            posicion = jug.posicion
+            costo = jug.costo
+        else:
+            success = 3 # No existe
+    
+    return render_template('jugador.html', nombre = nom, club = club, posicion = posicion, costo = costo, error=success )
+    #return str(personas[cod_id])
 
 @app.route('/ojeo')
 def ojeos():
-	return ojeos
+    return str(ojeos)
 
-@app.route('/ojeo/<int:cod_id>')
+@app.route('/ojeo/<int:cod_id>', methods=['GET', 'POST'])
 def ojeo(cod_id):
-	return ojeos[cod_id]
+    return ojeos[cod_id]
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
